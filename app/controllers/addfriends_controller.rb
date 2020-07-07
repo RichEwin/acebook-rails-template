bc2cab099a75e16be4d0b5cff08bf7d40a773938
J Perrin <perrinjack96@gmail.com>
2020-07-07 16:56:20 +0100

    updated example

--- app/controllers/addfriends_controller.rb
+++ app/controllers/addfriends_controller.rb
@@ -2,6 +2,7 @@ class AddfriendsController < ApplicationController
   def index
   # pull data from database
   params[:id] = session[:user_id]
+  @username = User.find_by(id:  session[:user_id])
   end
 
 end
